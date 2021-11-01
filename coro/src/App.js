a6dd8bcd0bfefc31cc4c76126e37bf67dd52b73f
jungjaelee <skskdi0324@naver.com>
2021-11-01 22:59:42 +0900

    메인 화면 구성(COVID-19)

---
+++ coro/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>COVID-19</h1>
+        <select>
+          <option>국내</option>
+          <option>해외</option>
+        </select>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
