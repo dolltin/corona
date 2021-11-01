b2a32f4e136d92838b8642b3989abb5d933f6609
jungjaelee <skskdi0324@naver.com>
2021-11-02 00:28:48 +0900

    chart 코딩중 에러 발생. 해결 예정

--- coro/src/components/Contents.js
+++ coro/src/components/Contents.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import {Bar, Doughnut, Line} from "react-chartjs2"
 import axios from 'axios'
 export const Contents = () => {
 
@@ -16,6 +17,12 @@ export const Contents = () => {
         <section>
         <h2>국내 코로나 현황</h2>
         <div className="contents"></div>
+            <div>
+                <Bar data = {confirmedData} options={
+                    { title: { display: true, text: "누적 확진자 추이", fontSize: 16}},
+                    {legend: {display: true, position: "bottom"}}
+                } />
+            </div>
         </section>
     )
 }
