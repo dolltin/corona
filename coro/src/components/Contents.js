957fe806632ec08356def954f869b37dfb15c635
jungjaelee <skskdi0324@naver.com>
2021-11-01 23:11:10 +0900

    코드 간략화를 위한 컴포넌트 분리2

---
+++ coro/src/components/Contents.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Contents = () => {
+    return (
+        <section>
+        <h2>국내 코로나 현황</h2>
+        <div className="contents"></div>
+        </section>
+    )
+}
